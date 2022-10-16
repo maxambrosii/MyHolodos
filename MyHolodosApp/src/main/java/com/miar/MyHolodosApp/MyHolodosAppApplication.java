7622e54b7fee81d4d100c15c0149974137b682e1
Maxim Ambrosii <c-maxim.ambrosii@edifecs.com>
2022-10-16 18:37:39 +0300

    Created project SpringBoot

---
+++ MyHolodosApp/src/main/java/com/miar/MyHolodosApp/MyHolodosAppApplication.java
@@ -0,0 +1,13 @@
+package com.miar.MyHolodosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHolodosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHolodosAppApplication.class, args);
+	}
+
+}
---
+++ MyHolodosApp/src/test/java/com/miar/MyHolodosApp/MyHolodosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miar.MyHolodosApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHolodosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
